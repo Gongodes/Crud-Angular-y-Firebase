7eeea07a06b463ee7057afcfc654e03e12c604d0
Gonzalo Godoy Espinosa <gonzalo.godoy.esp@gmail.com>
2022-09-26 10:25:57 -0300

    last update

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../environments/environment";
 import { Producto } from '../modelos/productos.models';
 import{map} from "rxjs/operators"
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ProductoService {
 
-  url:any=`${environment.url}productos.json`;
+  urlm= 'https://catalogo-productos-87ecc-default-rtdb.firebaseio.com/'
+
+  url:any=`${this.urlm}productos.json`;
 
   constructor(private http: HttpClient) { }
 
@@ -68,14 +71,14 @@ const prodAux = {...producto};
 
 delete prodAux.id;
 
-return this.http.put(`${environment.url}productos/${producto.id}.json`,prodAux)
+return this.http.put(`${this.urlm}productos/${producto.id}.json`,prodAux)
 
 }
 
 
 eliminarProducto(id:any){
 
- return this.http.delete(`${environment.url}productos/${id}.json`);
+ return this.http.delete(`${this.urlm}productos/${id}.json`);
 
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url:'https://catalogo-productos-87ecc-default-rtdb.firebaseio.com/'
+  
 };
 
 /*
